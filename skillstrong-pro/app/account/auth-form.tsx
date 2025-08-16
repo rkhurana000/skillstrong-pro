0b6d1eac15cef028e44860566aba60ce7e5e14ef
Rajeev Khurana <rkhurana000@gmail.com>
2025-08-16 12:54:48 -0700

    Create auth-form.tsx

---
+++ skillstrong-pro/app/account/auth-form.tsx
@@ -0,0 +1,22 @@
+// /app/account/auth-form.tsx
+'use client'
+
+import { Auth } from '@supabase/auth-ui-react'
+import { ThemeSupa } from '@supabase/auth-ui-shared'
+import { createClient } from '@/utils/supabase/client'
+
+export default function AuthForm() {
+  const supabase = createClient()
+
+  return (
+    <Auth
+      supabaseClient={supabase}
+      view="magic_link"
+      appearance={{ theme: ThemeSupa }}
+      theme="light"
+      showLinks={false}
+      providers={[]}
+      redirectTo={`${location.origin}/auth/callback`}
+    />
+  )
+}
