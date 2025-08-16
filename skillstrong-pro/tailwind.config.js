8b39af1cedda19510b846bc9970777c787f173a2
Rajeev Khurana <rkhurana000@gmail.com>
2025-08-15 19:25:11 -0700

    Update tailwind.config.js

--- skillstrong-pro/tailwind.config.js
+++ skillstrong-pro/tailwind.config.js
@@ -1,6 +1,19 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./app/**/*.{js,ts,jsx,tsx,mdx}", "./components/**/*.{js,ts,jsx,tsx,mdx}"],
-  theme: { extend: {} },
-  plugins: [],
+// tailwind.config.ts
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      // ...
+    },
+  },
+  plugins: [
+    require('@tailwindcss/typography'), // <-- ADD THIS LINE
+  ],
 };
+export default config;
