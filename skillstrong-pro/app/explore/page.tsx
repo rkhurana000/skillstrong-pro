fd834cf61d8078d73468b6745b8c2afc21b15b1f
Rajeev Khurana <rkhurana000@gmail.com>
2025-08-17 22:57:08 -0700

    Update page.tsx

--- skillstrong-pro/app/explore/page.tsx
+++ skillstrong-pro/app/explore/page.tsx
@@ -8,16 +8,5 @@ export default async function ExplorePage() {
     data: { user },
   } = await supabase.auth.getUser();
 
-  let initialLocation: string | null = null;
-  if (user) {
-    // If user is logged in, get location from their profile
-    const { data: profile } = await supabase
-      .from('profiles')
-      .select('zip_code')
-      .eq('id', user.id)
-      .single();
-    initialLocation = profile?.zip_code || null;
-  }
-
-  return <ExploreClient user={user} initialLocation={initialLocation} />;
+  return <ExploreClient user={user} />;
 }
